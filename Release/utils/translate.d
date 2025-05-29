f437be873935326f1aeff818b7a382cbaca28475
Shinjinok <bartshin00@gmail.com>
2025-05-29 10:07:49 +0900

    ignore

--- Release/utils/translate.d
+++
@@ -1,3 +0,0 @@
-utils/translate.o: ../utils/translate.c \
- /home/sjo/STM32CubeIDE/workspace_1.18.0/my1004c/deca_driver/deca_device_api.h
-/home/sjo/STM32CubeIDE/workspace_1.18.0/my1004c/deca_driver/deca_device_api.h:
